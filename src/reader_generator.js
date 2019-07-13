144825189ef76aac4faee5107d4ee4e546921956
Lachlan Phillips <flipsidelachy@gmail.com>
2019-07-13 17:37:49 +0930

    Add passage and rendering stats

--- src/reader_generator.js
+++ src/reader_generator.js
@@ -14,7 +14,7 @@ async function main () {
   const frequencyMap = await loadTokenFrequencyMap()
 
   const known = []
-  const chunkLength = 20
+  const chunkLength = 50
   const maxLearning = 5
 
   const p = await loadPassage({ paragraph_id: 635, start: 49467, end: 49541 })
@@ -31,6 +31,7 @@ async function main () {
   }
 
 
+  const allLearningWords = new Set()
   chunkArray(p.tokens, chunkLength).forEach(chunk => {
     const learningWords = _.chain(chunk)
       .filter(t => !known.includes(t.normalised))
@@ -42,6 +43,8 @@ async function main () {
       .take(maxLearning)
       .map(([_, t]) => t)
       .value()
+
+    learningWords.forEach(w => allLearningWords.add(w))
 
     for (const token of chunk) {
       if (learningWords.includes(token.normalised)) {
@@ -57,11 +60,26 @@ async function main () {
   }
 
 
+  const allWords = new Set()
+  const seenWords = new Set()
+  for (const token of p.tokens) {
+    const rendering = tokenRendering[token.token_id]
+    allWords.add(token.normalised)
+    if (rendering === 'native' || rendering === 'parallel') {
+      seenWords.add(token.normalised)
+    }
+  }
+
+
   const rendering = renderPassage(p, {
     divisionIndex: 'paragraph',
     tokenRendering,
   })
 
+  console.log('Learning:', [...allLearningWords].join(', '))
+  console.log('Learning ratio:', (allLearningWords.size / allWords.size * 100).toFixed(0) + '%')
+  console.log('Reveal ratio:', (seenWords.size / allWords.size * 100).toFixed(0) + '%')
+  console.log()
   console.log(formatAsText(rendering))
   console.log()
   console.log(formatAsHtml(rendering))
