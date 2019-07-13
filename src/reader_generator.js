86c136075e8a7f6ae57c212fcf9e717c3adddef3
Lachlan Phillips <flipsidelachy@gmail.com>
2019-07-13 17:43:30 +0930

    Extract renderer code

--- src/reader_generator.js
+++ src/reader_generator.js
@@ -1,8 +1,9 @@
-const wrap = require('wordwrap')(80)
 const chunkArray = require('lodash/chunk')
 const _ = require('lodash')
 
 const { loadPassage, loadTokenFrequencyMap, isProperNoun } = require('./ognt_dataset.js')
+const { renderPassage, formatAsText, formatAsHtml } = require('./renderer.js')
+
 
 main().catch(e => console.warn('error:', e))
 
@@ -79,117 +80,4 @@ async function main () {
   console.log(formatAsText(rendering))
   console.log()
   console.log(formatAsHtml(rendering))
-}
-
-
-/**
- * Renders a passage into blocks of rendered words.
- *
- * Options: {
- *   divisionIndex: String  name of the index to use for dividing the text
- *   tokenRendering: ID => String  type of rendering for each token
- * }
- */
-function renderPassage (passage, options) {
-  const invisiblePunctation = /[ ¶]+/g
-  const allPunctation = /[“”:?,.;·¶-]+/g
-
-  const blocks = divideBlocks(passage, options.divisionIndex || null)
-  return blocks
-    .map(b => formatBlock(b, options.tokenRendering || {}))
-
-
-  function divideBlocks (passage, divisionIndex) {
-    if (divisionIndex === null) return [passage.tokens]
-    const index = passage[divisionIndex + 's']
-
-    return index.map(slice => {
-      return passage.tokens.filter(t =>
-        t.token_id >= slice.start && t.token_id <= slice.end)
-    })
-  }
-
-
-  function formatBlock (tokens, tokenRendering) {
-    return tokens
-      .map(t => {
-        const native = t.word
-
-        const cleanTranslation = t.translation_study === '-' ? null : t.translation_study.replace(allPunctation, '').trim()
-        const translation = cleanTranslation || ''
-
-        const parallel = cleanTranslation
-          ? [t.word, cleanTranslation]
-          : [t.word]
-
-        const rendering = tokenRendering[t.token_id] || 'native'
-        let text
-        if (rendering === 'native') text = [native]
-        else if (rendering === 'translation') text = [translation]
-        else text = parallel
-
-        return [
-          t.punctuation_before.replace(invisiblePunctation, ''),
-          text,
-          t.punctuation_after.replace(invisiblePunctation, ''),
-        ]
-      })
-  }
-}
-
-function formatAsText (rendering) {
-  return rendering
-    .map(block => wrap(formatBlock(block)))
-    .join('\n\n')
-
-
-  function formatBlock (stream) {
-    return stream
-      .map(word => {
-        return word
-          .map(piece => {
-            if (Array.isArray(piece)) {
-              return piece.map((s, idx) => {
-                if (idx !== 0) return '(' + s + ')'
-                else return s
-              }).join(' ')
-            } else {
-              return piece
-            }
-          })
-          .join('')
-      })
-      .filter(text => !!text)
-      .join(' ')
-  }
-}
-
-function formatAsHtml (rendering) {
-  return rendering
-    .map(block => (
-      '<p style="font-size: 16pt; max-width: 50em; margin: auto; line-height: 1.6">' +
-      wrap(formatBlock(block)) +
-      '</p>'))
-    .join('\n')
-
-
-  function formatBlock (stream) {
-    return stream
-      .map(word => {
-        return word
-          .map(piece => {
-            if (Array.isArray(piece) && piece.length > 1) {
-              return (
-                '<span style="display: inline-block; text-align: center; padding-bottom: 12px">' +
-                piece.filter(p => p).map((p, idx) => idx > 0 ? `<span style="color: grey; line-height: 1">${p}</span>` : p).join('<br>') +
-                '</span>')
-            } else if (piece !== '') {
-              return '<span style="vertical-align: top">' + piece + '</span>'
-            }
-          })
-          .join('')
-      })
-      .filter(text => !!text)
-      .join(' ')
-  }
 }
--- src/renderer.js
+++ src/renderer.js
@@ -1,65 +1,121 @@
-/**
- * Renderer -- takes a list of text/interlinear blocks and formats them as HTML
- * or text.
- *
- * Usage: node renderer.js <html|text>
- */
-
 const wrap = require('wordwrap')(80)
-const { readStdin } = require('./utils')
 
-if (process.argv.length !== 3) {
-  console.warn('Usage: node renderer.js <html|text>')
-  return
+
+/**
+ * Renders a passage into blocks of rendered words.
+ *
+ * Options: {
+ *   divisionIndex: String  name of the index to use for dividing the text
+ *   tokenRendering: ID => String  type of rendering for each token
+ * }
+ */
+function renderPassage (passage, options) {
+  const invisiblePunctation = /[ ¶]+/g
+  const allPunctation = /[“”:?,.;·¶-]+/g
+
+  const blocks = divideBlocks(passage, options.divisionIndex || null)
+  return blocks
+    .map(b => formatBlock(b, options.tokenRendering || {}))
+
+
+  function divideBlocks (passage, divisionIndex) {
+    if (divisionIndex === null) return [passage.tokens]
+    const index = passage[divisionIndex + 's']
+
+    return index.map(slice => {
+      return passage.tokens.filter(t =>
+        t.token_id >= slice.start && t.token_id <= slice.end)
+    })
+  }
+
+
+  function formatBlock (tokens, tokenRendering) {
+    return tokens
+      .map(t => {
+        const native = t.word
+
+        const cleanTranslation = t.translation_study === '-' ? null : t.translation_study.replace(allPunctation, '').trim()
+        const translation = cleanTranslation || ''
+
+        const parallel = cleanTranslation
+          ? [t.word, cleanTranslation]
+          : [t.word]
+
+        const rendering = tokenRendering[t.token_id] || 'native'
+        let text
+        if (rendering === 'native') text = [native]
+        else if (rendering === 'translation') text = [translation]
+        else text = parallel
+
+        return [
+          t.punctuation_before.replace(invisiblePunctation, ''),
+          text,
+          t.punctuation_after.replace(invisiblePunctation, ''),
+        ]
+      })
+  }
 }
 
-const styles = `
-* {
-  max-width: 60ch;
+function formatAsText (rendering) {
+  return rendering
+    .map(block => wrap(formatBlock(block)))
+    .join('\n\n')
+
+
+  function formatBlock (stream) {
+    return stream
+      .map(word => {
+        return word
+          .map(piece => {
+            if (Array.isArray(piece)) {
+              return piece.map((s, idx) => {
+                if (idx !== 0) return '(' + s + ')'
+                else return s
+              }).join(' ')
+            } else {
+              return piece
+            }
+          })
+          .join('')
+      })
+      .filter(text => !!text)
+      .join(' ')
+  }
 }
-html {
-  font-family: sans-serif;
-  font-size: 14pt;
-  max-width: initial;
-  width: 100%;
+
+function formatAsHtml (rendering) {
+  return rendering
+    .map(block => (
+      '<p style="font-size: 16pt; max-width: 50em; margin: auto; line-height: 1.6">' +
+      wrap(formatBlock(block)) +
+      '</p>'))
+    .join('\n')
+
+
+  function formatBlock (stream) {
+    return stream
+      .map(word => {
+        return word
+          .map(piece => {
+            if (Array.isArray(piece) && piece.length > 1) {
+              return (
+                '<span style="display: inline-block; text-align: center; padding-bottom: 12px">' +
+                piece.filter(p => p).map((p, idx) => idx > 0 ? `<span style="color: grey; line-height: 1">${p}</span>` : p).join('<br>') +
+                '</span>')
+            } else if (piece !== '') {
+              return '<span style="vertical-align: top">' + piece + '</span>'
+            }
+          })
+          .join('')
+      })
+      .filter(text => !!text)
+      .join(' ')
+  }
 }
-body {
-  margin: 2em auto;
+
+
+module.exports = {
+  renderPassage,
+  formatAsText,
+  formatAsHtml,
 }
-`
-
-const format = process.argv[2]
-const prologue = format === 'text' ? '' : '<html>\n<body>\n'
-const joiner = format === 'text' ? '\n\n-------\n\n' : '\n<hr>\n'
-const epilogue = format === 'text' ? '' : '\n<style>' + styles + '</style></body>\n</html>'
-
-const blocks = readStdin()
-  .split('\n')
-  .map(text => text.trim())
-  .filter(t => t)
-  .map(text => JSON.parse(text))
-
-const formatted = blocks
-  .map(block => {
-    const centerIndex = (block.text.length / 2) | 0
-
-    if (format === 'text') {
-      const text = wrap(highlight(block.text).join(' | '))
-      const interlinear = wrap(highlight(block.interlinear).join(' '))
-      return text + '\n\n' + interlinear
-    } else {
-      const text = wrap(highlight(block.text).join(' | '))
-      const interlinear = wrap(highlight(block.interlinear).join(' '))
-      return '<p>' + text + '</p>\n<p>' + interlinear + '</p>'
-    }
-
-    function highlight (array) {
-      if (format === 'text') {
-        return array.map((s, idx) => idx === centerIndex ? `*${s}*` : s)
-      } else {
-        return array.map((s, idx) => idx === centerIndex ? `<strong>${s}</strong>` : s)
-      }
-    }
-  })
-
-console.log(prologue + formatted.join(joiner) + epilogue)
