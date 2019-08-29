418d4f3ccdbdc24bc106da22b4b04870d21fba51
Lachlan Phillips <flipsidelachy@gmail.com>
2019-08-29 21:04:57 +0930

    Load Greek for learning words

--- src/main.js
+++ src/main.js
@@ -7,34 +7,117 @@ const ISLAND = ['1 John']
 const EXTENSIVE_WORD_COUNT = 100
 
 
+const zipWith = require('lodash/zipWith')
+
+const {
+  loadPersistentState,
+  executeWithPersistentState,
+} = require('./persistence.js')
+
+const {
+  loadPassage,
+  loadPassageIndex,
+  loadIndexByIds,
+  loadTokenFrequencyMap,
+  isProperNoun,
+} = require('./ognt_dataset.js')
+
+
 main(process.argv.slice(2)).catch(e => console.warn('error:', e))
 
 async function main (args) {
-  // PUMP FEEDER
+  await pumpFeeder()
+
+  const lesson = await loadLesson()
+
+  // RENDER LESSON
+  // load verse text (ESV, greek, mixed)
+  // format lesson
+  const text = renderLessonText(lesson)
+
+  // POST PROCESSING
+  // CSS inlining
+
+  console.log(text)
+}
+
+
+async function pumpFeeder () {
   // progress to recall
   // add new learn
   // add new achieved verses
+}
 
-  // LOAD LESSON
+
+async function loadLesson () {
   // learning
   // recall scheduler
   // load achieved verses and near achieved
   // load reading passage
   // calculate stats
-  const lesson = makeLesson()
 
-  // RENDER LESSON
-  // load verse text (ESV, greek, mixed)
-  // format lesson
+  // STATE
+  // learning: [{
+  //   word: 'tau'
+  //   definition: ''
+  //   translations: []
+  //   examples: ['390423']
+  //   timesSeen: 2
+  // }]
+  //
+  // recall: [{
+  //   word: 'tau'
+  //   examples: [...]
+  //   timesRecalled
+  //   nextRecall
+  // }]
+  //
+  // knownWords: ['tau', ...]
+  //
+  // achieved: [{
+  //   reference: '...'
+  //   achievedOn
+  // }]
+  //
+  // extensiveReading: next token ID
 
-  // POST PROCESSING
-  // CSS inlining
+  const learningWords = [
+    {
+      word: 'τρεῖς',
+      definition: 'three, etc etc',
+      translations: ['three'],
+      examples: [
+        '40012040',
+        '42002046',
+        '59005017',
+      ],
+    },
+  ]
 
-  console.log(renderLessonText(lesson))
-}
+  const knownWords = ['τρεῖς']
 
 
-function makeLesson () {
+  for (const learningWord of learningWords) {
+    // TODO: only take a selection of examples
+    const indexes = await loadIndexByIds('verse', learningWord.examples.map(e => {
+      return e.slice(0, 2) + '-' + parseInt(e.slice(2, 5)) + '-' + parseInt(e.slice(5))
+    }))
+
+    const verses = await Promise.all(indexes.map(i => loadPassage(i)))
+    const english = learningWord.examples
+
+    const formatted = zipWith(verses, english).map(([verse, english]) => {
+      return {
+        reference: calculateReference(verse),
+        text: verse.tokens.map(v => v.word).join(' '),
+        mixed: verse.tokens.map(v => knownWords.includes(v.word) ? v.word : v.translation_study).join(' '),
+        translation: english,
+      }
+    })
+
+    learningWord.examples = formatted
+  }
+
   return {
     date: new Date(),
     recall: [
@@ -62,21 +145,7 @@ function makeLesson () {
         ],
       },
     ],
-    learn: [
-      {
-        word: 'ksero',
-        definition: 'I know, as in, I know where he is / ksero pou ine',
-        translations: ['know', 'I know', 'am knowing'],
-        examples: [
-          {
-            reference: '1 Timothy 2:8',
-            text: '',
-            mixed: '',
-            translation: '',
-          },
-        ],
-      },
-    ],
+    learn: learningWords,
     achievedVerses: [
       {
         fraction: 1,
@@ -174,4 +243,58 @@ function formatDate (d) {
   return '' + d.getFullYear() +
     ' ' + ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][d.getMonth()] +
     ' ' + d.getDate()
+}
+
+function calculateReference (passage) {
+  if (passage.verses.length === 0) return 'N/A'
+
+  const books = {
+    40: 'Matthew',
+    41: 'Mark',
+    42: 'Luke',
+    43: 'John',
+    44: 'Acts',
+    45: 'Romans',
+    46: '1 Corinthians',
+    47: '2 Corinthians',
+    48: 'Galatians',
+    49: 'Ephesians',
+    50: 'Philippians',
+    51: 'Colossians',
+    52: '1 Thessalonians',
+    53: '2 Thessalonians',
+    54: '1 Timothy',
+    55: '2 Timothy',
+    56: 'Titus',
+    57: 'Philemon',
+    58: 'Hebrews',
+    59: 'James',
+    60: '1 Peter',
+    61: '2 Peter',
+    62: '1 John',
+    63: '2 John',
+    64: '3 John',
+    65: 'Jude',
+    66: 'Revelation',
+  }
+
+  const startId = passage.verses[0].verse_id.split('-')
+  const endId = passage.verses[passage.verses.length - 1].verse_id.split('-')
+
+  if (startId.join('') === endId.join('')) return formatVerseId(startId)
+  else return formatVerseId(startId) + '-' + formatVerseId(endId, startId)
+
+
+  function formatVerseId (id, diff) {
+    const [book, chapter, verse] = id
+    const [diffBook, diffChapter] = diff || [null, null, null]
+
+    if (book !== diffBook) {
+      return `${books[book]} ${chapter}:${verse}`
+    } else if (chapter !== diffChapter) {
+      return `${chapter}:${verse}`
+    } else {
+      return `${verse}`
+    }
+  }
 }
